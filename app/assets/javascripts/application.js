c6e502d38c4d85d08da56a39afbd247ec010831f
Mythrex <shivam.kaushik73@gmail.com>
2018-03-08 16:41:08 +0530

    Add Login Page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require jquery
-//= require materialize-sprockets
 //= require rails-ujs
 //= require turbolinks
+//= require materialize
 //= require_tree .
